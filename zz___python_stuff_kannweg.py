404053f58f8a890741c4c7ffaa602810feae51d7
ldpgh <ldpgh@users.noreply.github.com>
2025-12-30 17:25:43 +0100

    Update zz___python_stuff_kannweg.py
    fix dirName content

--- zz___python_stuff_kannweg.py
+++ zz___python_stuff_kannweg.py
@@ -25,7 +25,7 @@ from a2p_importedPart_class import Proxy_importPart, ImportedPartViewProviderPro
 App.newDocument()
 doc=FreeCAD.activeDocument()
 
-dirName='./FreeCAD/A2P_HIER_PARAMETER/'
+dirName='%APPDATA%/FreeCAD/A2plus_design_files/'
 a2p.importPartFromFile( doc, dirName+'hier_level_2.FCStd', instanceParameters={"ww":55} )
 
 
@@ -46,13 +46,13 @@ a2p_importedPart_class
 
 a2p.importPartFromFile(
   doc,
-  './FreeCAD/A2P_HIER_PARAMETER/hier_level_2.FCStd',
+  dirName+"hier_level_2.FCStd',
   instanceParameters={"ww":11, "ll":11, "hh":11}
 )
 
 a2p.importPartFromFile(
   doc,
-  './FreeCAD/A2P_HIER_PARAMETER/hier_level_2.FCStd',
+  dirName+"hier_level_2.FCStd',
   instanceParameters={"ww":11, "ll":22, "hh":33}
 )
 
@@ -99,4 +99,5 @@ doc = FreeCAD.activeDocument() or FreeCAD.newDocument()
 obj = doc.addObject("Part::FeaturePython", "MyPart")
 ProxyDemo(obj)
 doc.recompute()
+
 
